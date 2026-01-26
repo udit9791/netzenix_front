cab877d2187a5cfdf9c061c2d1753beb4606c4cb
udit9791 <uditprajapati9791@gmail.com>
2026-01-26 17:08:25 +0530

    https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost/deploye/public/api',
-  imgUrl: 'http://localhost/deploye/public/',
+  // apiUrl: 'http://localhost/deploye/public/api',
+  //imgUrl: 'http://localhost/deploye/public/',
   //production
 
-  //apiUrl: 'http://159.89.161.235/v1/api',
-  //imgUrl: 'http://159.89.161.235/v1/public/',
+  apiUrl: 'https://159.89.161.235/v1/api',
+  imgUrl: 'https://159.89.161.235/v1/public/',
   googleMapsApiKey: 'AIzaSyCG5IEOSZwoY0LlVnS0EtOjV5cR7OjWevI'
 };
